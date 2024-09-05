12884f16b465bef4603a56450d69e3ccf6cbf10f
sameer3841 <81244405+sameer3841@users.noreply.github.com>
2024-09-05 15:48:13 -0400

    Added certifications and projects

--- send_email.php
+++ send_email.php
@@ -8,10 +8,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $to = "your.email@example.com"; // Replace with your email address
     $email_subject = "New Contact Form Submission: " . $subject;
     $email_body = "You have received a new message from the contact form.\n\n".
-                  "Here are the details:\n".
-                  "Name: $name\n".
-                  "Email: $email\n\n".
-                  "Message:\n$message";
+                "Here are the details:\n".
+                "Name: $name\n".
+                "Email: $email\n\n".
+                "Message:\n$message";
 
     $headers = "From: noreply@yourdomain.com\n"; // Replace with your domain
     $headers .= "Reply-To: $email";
